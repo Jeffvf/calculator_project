152434b38d6b768edb449e904992d2ccfa9f334d
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-08-14 18:00:34 -0300

    feat: Add operate function

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 
 const divide = (a,b) => a / b;
+
+const operate = (operator, a, b) => {
+    if(operator == '+'){
+        return add(a,b);
+    }
+
+    else if(operator == '-'){
+        return subtract(a,b);
+    }
+
+    else if(operator == '*'){
+        return multiply(a,b);
+    }
+
+    else if(operator == '/'){
+        return divide(a,b);
+    }
+
+    else{
+        return 'Invalid operator'
+    }
+};
